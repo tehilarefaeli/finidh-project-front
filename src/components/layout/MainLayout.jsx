d953b7d9451f3f35c6e1a92280419212c0f112b8
shilat <shilats2001@gmail.com>
2024-01-15 21:05:30 +0200

    mainpage

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,13 +2,15 @@ import { Layout } from 'antd';
 import React from 'react';
 import Footers from '../common/Footers';
 import Headers from '../common/Headers';
+import Homepage from '../../pages/homePage';
 
 const { Content } = Layout;
 
 function MainLayout({ children }) {
   return (
     <Layout>
-      
+      <Homepage/>
+      <Footers/>
       {/* <Content className='min-h-[75vh]'>{ children }</Content> */}
       {/* <Footers /> */}
     </Layout>
