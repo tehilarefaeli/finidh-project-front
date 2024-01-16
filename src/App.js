c276e99a68d24b65889581a249f4ddd53bc62d81
SariLev <sarile200@gmail.com>
2024-01-16 22:03:55 +0200

    buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Profile from './pages/Profile';
 import Cakes from './pages/Cakes';
 import Deserts from './pages/Deserts';
 import Headers from './components/common/Headers';
+import Wantingred from './pages/Wantingredients';
+import All from './pages/Allrecipes';
+import Noingred from './pages/Noingredients';
 
 
 
@@ -23,9 +26,13 @@ function App() {
         <Route path='/cakes' element={<Cakes />} />
         <Route path='/cookies' element={<Cookies />} />
         <Route path='/deserts' element={<Deserts />} />
+        <Route path='/Wantingredints' element={<Wantingred />} />
+        <Route path='/Noingredints' element={<Noingred />} />
+        <Route path='/all' element={<All />} />
         <Route path='*' element={<NotFound />} />
      
       </Routes>
+      // להוסיף פוטר
     </BrowserRouter>
   );
 }
