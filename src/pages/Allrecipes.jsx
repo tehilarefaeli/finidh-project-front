2b0afa4b18dbbd7326724fb424d874814fca119d
Rachelikl <2210ksr@gmail.com>
2024-01-20 20:52:11 +0200

    SpeakWithBackToGetRecipies

--- src/pages/Allrecipes.jsx
+++ src/pages/Allrecipes.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import GetRequest from "../helpers/getRequest";
 
-function All() {
+
+function AllRecipres() {
+ // const recipes = GetRequest("recipies/all")
+ const [recipes,setrecipes] = useState();
+  useEffect(()=>{
+  GetRequest("recipies/all").then(res => {
+  console.log("useEffect", res);
+  setrecipes(res)
+}
+).catch(e => console.log(e))
+
+ },[]);
+
 
   return (
     <div>
-      i am recipes feed!
+      {recipes && recipes[0]?.recipe_name}
     </div>
   );
 }
 
-export default All;
+export default AllRecipres;
