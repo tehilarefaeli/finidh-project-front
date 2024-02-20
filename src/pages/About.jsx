7753eb1bcbe5077b8f28b0843253300a03cf18a9
SariLev <sarile200@gmail.com>
2024-02-20 20:03:29 +0200

    j

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,49 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import About from './pages/About'
-import Cookies from './pages/Cookies';
-import Profile from './pages/Profile';
-import Cakes from './pages/Cakes';
-import Deserts from './pages/Deserts';
-import Headers from './components/common/Headers';
-import Wantingred from './pages/Wantingredients';
-import All from './pages/Allrecipes';
-//import Noingred from './pages/Noingredients';
-import Login from './pages/Login';
-import Footers from './components/common/Footers';
-import Signup from './pages/signup';
-import Recipe from './pages/recipe';
-import FilterResult from './pages/FilterResult';
+import BasicButton from '../components/basic/BasicButton';
 
-function App() {
+function About() {
 
   return (
-    
-    <BrowserRouter>
-    <Headers />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/cakes' element={<Cakes />} />
-        <Route path='/cookies' element={<Cookies />} />
-        <Route path='/deserts' element={<Deserts />} />
-        <Route path='/Wantingredints' element={<Wantingred />} />
-        <Route path='/all' element={<All />} />
-        <Route path='/recipe' element={<Recipe />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/FilterResult' element={<FilterResult/>}/>
-
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-      <Footers/>
-    </BrowserRouter>
-
+    <div>
+      For desserts, our site is an advanced recipe site.
+There are many recipe apps and websites on the market today, including
+of famous chefs
+We came up with a brilliant solution that will give users a different level of experience
+the site.
+When people enter the kitchen to prepare something, they often encounter
+In a frustrating situation of 'whoops, I don't have all the materials'
+Then either the grocery store is far away or it's late and here's a username
+The site abandons the recipe with a bitter taste in its mouth
+And a mother of an allergic child who builds on some irritating recipe and in the end finds out that it is
+Contains a component that is dangerous for the girl
+Our site enters exactly this niche and offers a solution:
+First of all let us know what your available supplies are/are not
+are relevant - and according to this we will give you the recipes that suit you.
+No more surfing and flipping through dozens of irrelevant recipes and feeling hungry
+pinching From today you receive only relevant and practical recipes!
+In addition, we offer the user to receive an indication of the rating of the recipe,
+When a user sees a high rating it gives him confidence and certainty about
+Choosing the recipe
+There will be additional filter options in order to make the adjustment of the recipe more accurate
+to the seeker
+And of course a personal area that will be the cache of all the recipes you liked
+We set a goal before our eyes to give our user an efficient browsing experience
+Effective and of course tasty
+    </div>
   );
-
 }
 
-export default App;
+export default About;
